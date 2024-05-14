d132f25601fce74b67b1da654bbfdb05a3662fe6
Cowboy Crypto <dwight@cowboycrypto.io>
2024-05-14 13:36:21 -0700

    Brand
    Brand section complete

--- src/components /brand/Brand.jsx
+++ src/components /brand/Brand.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './brand.css';
-
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt4__brand section__padding'>
+      <div>
+        <img src={google} alt="google"></img>
+      </div>
+      <div>
+        <img src={slack} alt="slack"></img>
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassin"></img>
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox"></img>
+      </div>
+      <div>
+        <img src={shopify} alt="shopify"></img>
+      </div>
+    </div>
   )
 }
 
