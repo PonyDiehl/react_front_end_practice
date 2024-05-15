2b4fc558ac41950ebc5206f616f9750f5ce89ca7
Cowboy Crypto <dwight@cowboycrypto.io>
2024-05-14 19:31:14 -0700

    What-gpt4
    Completed what section

--- src/containers/whatGPT4/WhatGPT4.jsx
+++ src/containers/whatGPT4/WhatGPT4.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './whatGPT4.css';
+import Feature from '../../components /feature/Feature';
 
 const WhatGPT4 = () => {
   return (
-    <div>WhatGPT4</div>
+    <div className="gpt4__whatgpt4 section__margin" id="wgpt4">
+      <div className='gpt4__whatgpt4-feature'>
+        <Feature title="What is GPT-4" text="I'm baby raw denim paleo big mood, chambray tattooed beard pickled cornhole blackbird spyplane. Selvage JOMO cliche pickled. Tilde stumptown disrupt marxism pug, venmo polaroid meditation XOXO retro pickled blog etsy kombucha truffaut. Next level blog poutine locavore, green juice YOLO salvia hashtag cronut tacos shabby chic bushwick helvetica. Four dollar toast JOMO flexitarian farm-to-table, bicycle rights coloring book blackbird spyplane jean shorts shaman enamel pin. Shaman lyft vexillologist meh tacos. Vaporware YOLO +1, keytar cold-pressed cloud bread letterpress scenester everyday carry."/>
+      </div>
+      <div className='gpt4__whatgpt4-heading'>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt4__whatgpt4-container'>
+        <Feature title="Chatbots" text="I'm baby raw denim paleo big mood, chambray tattooed beard pickled cornhole blackbird spyplane. Selvage JOMO cliche pickled. Tilde stumptown disrupt marxism pug, venmo polaroid meditation XOXO retro pickled blog etsy kombucha truffaut. Next level blog poutine locavore, green juice YOLO salvia hashtag cronut tacos shabby chic bushwick helvetica. Four dollar toast JOMO flexitarian farm-to-table, bicycle rights coloring book blackbird spyplane jean shorts shaman enamel pin. Shaman lyft vexillologist meh tacos. Vaporware YOLO +1, keytar cold-pressed cloud bread letterpress scenester everyday carry."/>
+        <Feature title="Knowledgebase" text="I'm baby raw denim paleo big mood, chambray tattooed beard pickled cornhole blackbird spyplane. Selvage JOMO cliche pickled. Tilde stumptown disrupt marxism pug, venmo polaroid meditation XOXO retro pickled blog etsy kombucha truffaut. Next level blog poutine locavore, green juice YOLO salvia hashtag cronut tacos shabby chic bushwick helvetica. Four dollar toast JOMO flexitarian farm-to-table, bicycle rights coloring book blackbird spyplane jean shorts shaman enamel pin. Shaman lyft vexillologist meh tacos. Vaporware YOLO +1, keytar cold-pressed cloud bread letterpress scenester everyday carry."/>
+        <Feature title="Education" text="I'm baby raw denim paleo big mood, chambray tattooed beard pickled cornhole blackbird spyplane. Selvage JOMO cliche pickled. Tilde stumptown disrupt marxism pug, venmo polaroid meditation XOXO retro pickled blog etsy kombucha truffaut. Next level blog poutine locavore, green juice YOLO salvia hashtag cronut tacos shabby chic bushwick helvetica. Four dollar toast JOMO flexitarian farm-to-table, bicycle rights coloring book blackbird spyplane jean shorts shaman enamel pin. Shaman lyft vexillologist meh tacos. Vaporware YOLO +1, keytar cold-pressed cloud bread letterpress scenester everyday carry."/>
+      </div>
+    </div>
   )
 }
 
